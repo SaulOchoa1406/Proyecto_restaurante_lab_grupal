6c9e6190853e1e0965a7047dea30ee6d2c758501
SaulOchoa1406 <sochoar@unsa.edu.pe>
2026-07-16 18:08:57 -0500

    creación de modelos categoria y productos para organizar mejor el inventario

--- inventory/models.py
+++ inventory/models.py
@@ -9,3 +9,26 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.nombre
+
+
+class Producto(models.Model):
+
+    nombre = models.CharField(max_length=150)
+
+    descripcion = models.TextField(blank=True)
+
+    precio = models.DecimalField(
+            max_digits=8,
+            decimal_places=2
+            )
+
+    categoria = models.ForeignKey(
+            Categoria,
+            on_delete=models.PROTECT,
+            related_name="productos"
+            )
+
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
