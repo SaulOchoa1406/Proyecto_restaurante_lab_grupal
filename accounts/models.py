ab9fa34974b9eaf1e2618f26a5d5301eeaf203f9
SaulOchoa1406 <sochoar@unsa.edu.pe>
2026-07-16 14:37:22 -0500

    creación del modelo usser

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Usuario(AbstractUser):
+
+    class Roles(models.TextChoices):
+        MOZO = "MOZO", "Mozo"
+        COCINERO = "COCINERO", "Cocinero"
+        ADMIN = "ADMIN", "Administrador"
+
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+
+    rol = models.CharField(
+            max_length=20,
+            choices=Roles.choices,
+            default=Roles.MOZO
+    )
+
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
