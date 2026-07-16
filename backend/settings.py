8ee9b08f462a41931864844f1804ffdd12613db9
SaulOchoa1406 <sochoar@unsa.edu.pe>
2026-07-16 14:18:47 -0500

    corrección error

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'rest_frameworf_simplejwt',
+    'rest_framework_simplejwt',
 
     'accounts',
     'restaurant',
