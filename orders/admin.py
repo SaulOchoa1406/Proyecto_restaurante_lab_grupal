dccfe218c8491211dd67fe1661131e26b0accf56
SaulOchoa1406 <sochoar@unsa.edu.pe>
2026-07-16 20:25:18 -0500

    detalles para el panel admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 from .models import Pedido, DetallePedido
 
-admin.site.register(Pedido)
-admin.site.register(DetallePedido)
+class DetallePedidoInline(admin.TabularInline):
+    model = DetallePedido
+    extra = 0
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = (
+            "id",
+            "mesa",
+            "mozo",
+            "estado",
+            "total",
+            "fecha",
+            )
+
+    list_filter = (
+            "estado",
+            )
+
+    ordering = (
+            "-fecha",
+            )
+
+    inlines = [
+            DetallePedidoInline
+            ]
