9148d272bce7943178106de91f4caaed30723968
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-19 22:15:00 -0500

    proyecto angular y componentes y servicios ncesarios

---
+++ frontend/angular/src/app/features/waiter/order-sumary/order-sumary.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-sumary',
+  imports: [],
+  templateUrl: './order-sumary.html',
+  styleUrl: './order-sumary.css',
+})
+export class OrderSumary {}
---
+++ frontend/angular/src/app/features/waiter/table-detail/table-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-detail',
+  imports: [],
+  templateUrl: './table-detail.html',
+  styleUrl: './table-detail.css',
+})
+export class TableDetail {}
