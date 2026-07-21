b1639905bbf531aea7d28023eb2eaf797303d587
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-20 23:33:41 -0500

    configuración de accesibilidad de restframework

--- backend/settings.py
+++ backend/settings.py
@@ -131,7 +131,10 @@ REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': (
             'rest_framework_simplejwt.authentication.JWTAuthentication',
             ),
-        }
+        'DEFAULT_PERMISSION_CLASSES': (
+            'rest_framework.permissions.IsAuthenticated',
+        )
+    }
 
 # Indicación a Django para que el usuario sea Usuario
 
