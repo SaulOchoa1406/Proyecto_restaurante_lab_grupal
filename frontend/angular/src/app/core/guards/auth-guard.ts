bebaebe24ec92f9b5a17df249d6648a5833f784f
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-21 09:21:52 -0500

    guard del usuario

--- frontend/angular/src/app/core/guards/auth-guard.ts
+++ frontend/angular/src/app/core/guards/auth-guard.ts
@@ -1,5 +1,15 @@
-import { CanMatchFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanMatchFn, Router } from '@angular/router';
 
-export const authGuard: CanMatchFn = (route, segments) => {
-  return true;
+export const authGuard: CanMatchFn = () => {
+  const router = inject(Router);
+  const token = localStorage.getItem("access");
+
+  if (token) {
+    return true;
+  }
+
+  router.navigate(['/login']);
+
+  return false;
 };
