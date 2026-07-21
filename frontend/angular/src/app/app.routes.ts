abf2db0d2c7dd4662f923877385186e8218b3af1
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-21 09:29:50 -0500

    rutas agregadas

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { Dashboard } from './features/admin/dashboard/dashboard';
+import { authGuard } from './core/guards/auth-guard';
+import { roleGuard } from './core/guards/role-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'login',
+    component: Login
+  },
+
+  {
+    path: 'register',
+    component: Register
+  },
+
+  {
+    path: 'waiter',
+    component: Dashboard,
+    canActivate: [
+      authGuard,
+      roleGuard('MOZO')
+    ]
+  },
+
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
