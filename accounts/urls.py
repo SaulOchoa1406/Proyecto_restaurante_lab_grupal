1bbd2045577401af9465838b39035e188d6b1875
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-21 00:14:15 -0500

    rutas para la autenticación

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import include, path
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from .views import RegisterView, CurrentUserView, LogoutView
 
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view()),
-    path('token/refresh/', TokenRefreshView.as_view()),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('me/', CurrentUserView.as_view(), name='current_user'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
 ]
