8b9fb71c493303382a4ffb36c29f991420d57316
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-21 09:18:47 -0500

    interceptor de usuario. Captura automaticamente los tokens

--- frontend/angular/src/app/core/interceptors/auth-interceptor.ts
+++ frontend/angular/src/app/core/interceptors/auth-interceptor.ts
@@ -1,5 +1,18 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const accesToken = localStorage.getItem("acces");
+
+  // Verifica si hay un acces token, sino no hace nada
+  if(!accesToken) {
+    return next(req);
+  }
+
+  const authRequest = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${accesToken}`
+    }
+  });
+
+  return next(authRequest);
 };
