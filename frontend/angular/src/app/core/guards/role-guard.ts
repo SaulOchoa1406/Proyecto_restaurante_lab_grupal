3bcc1b08485e4f21d5168535c3de03e3fccdd655
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-21 09:27:09 -0500

    guard del rol

--- frontend/angular/src/app/core/guards/role-guard.ts
+++ frontend/angular/src/app/core/guards/role-guard.ts
@@ -1,5 +1,32 @@
-import { CanMatchFn } from '@angular/router';
+import { inject } from '@angular/core';
+import {  CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { catchError, map, of } from 'rxjs';
 
-export const roleGuard: CanMatchFn = (route, segments) => {
-  return true;
+export const roleGuard = (rolPermitido: string): CanActivateFn => {
+  return () => {
+
+    const authService = inject(AuthService);
+    const router = inject(Router);
+
+    return authService.getCurrentUser().pipe(
+      map(user => {
+        if (user.rol === rolPermitido) {
+          return true;
+        }
+
+        router.navigate(['/login']);
+        
+        return false;
+      }),
+
+      catchError(() => {
+        router.navigate(['/login']);
+
+        return of(false);
+      })
+      
+    );
+
+  };
 };
