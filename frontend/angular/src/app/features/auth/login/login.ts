b4cb7e96d2c22ca28409298f7e98382a422565da
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-21 09:42:25 -0500

    lógica del login

--- frontend/angular/src/app/features/auth/login/login.ts
+++ frontend/angular/src/app/features/auth/login/login.ts
@@ -1,9 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule, 
+    RouterModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  
+  private fb = inject(FormBuilder);
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  loading = false;
+
+  error = '';
+
+  form = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  login() {
+    if (this.form.invalid) return;
+
+    this.loading = true;
+    const username = this.form.value.username!;
+    const password = this.form.value.password!;
+
+    this.auth.login(username, password).subscribe({
+      next: () => {
+        this.auth.getCurrentUser().subscribe({
+          next: (user) => {
+            switch (user.rol) {
+              case "MOZO":
+                this.router.navigate(['/waiter']);
+                break;
+              
+              case "ADMINISTRADOR":
+                this.router.navigate(['/admin']);
+                break;
+              
+              case "COCINERO":
+                this.router.navigate(['/kitchen']);
+                break;
+            }
+          }
+        });
+      },
+
+      error: () => {
+        this.loading = false;
+        this.error = "Usuario o contraseña incorrectos";
+      }
+
+    });
+  }
+
+}
