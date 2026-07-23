02a3a3c86c859fa85fb945ea23f5279fc926b388
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-23 17:31:01 -0500

    servicio table para las mesas

--- frontend/angular/src/app/core/services/table.ts
+++ frontend/angular/src/app/core/services/table.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { Table } from '../models/table';
 
 @Injectable({
     providedIn: 'root'
 })
 export class TableService {
-    constructor() {}
+    private readonly http = inject(HttpClient);
+    private readonly api = environment.apiUrl;
+
+    list(): Observable<Table[]> {
+        return this.http.get<Table[]>(`${this.api}/tables/mesas/`);
+    }
+
+    get(id: number): Observable<Table> {
+        return this.http.get<Table>(`${this.api}/tables/mesas/${id}/`);
+    }
 }
