2c25b7c7b678f8c4edb6d7de0942c153061042c4
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-22 20:41:37 -0500

    lógica y template para los usuarios

--- frontend/angular/src/app/features/admin/users/users.ts
+++ frontend/angular/src/app/features/admin/users/users.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject, model } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UsersService } from '../../../core/services/users';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './users.html',
   styleUrl: './users.css',
 })
-export class Users {}
+export class Users {
+  private readonly fb = inject(FormBuilder);
+  private readonly usersServices = inject(UsersService);
+
+  loading = false;
+  error = '';
+  success = '';
+
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required, Validators.minLength(8)],
+    first_name: ['', Validators.required],
+    last_name: ['', Validators.required],
+    rol: ['MOZO' as 'MOZO' | 'COCINERO',  Validators.required],
+  });
+
+  createUser(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.loading = true;
+    this.error = '';
+    this.success = '';
+
+    this.usersServices.create(this.form.getRawValue()).subscribe({
+      next: () => {
+        this.success = 'Usuario creado correctamente',
+        this.form.reset({ rol: 'MOZO' });
+        this.loading = false;
+      },
+
+      error: () => {
+        this.error = 'No se pudo crear el usuario',
+        this.loading = false;
+      },
+    });
+  }
+}
