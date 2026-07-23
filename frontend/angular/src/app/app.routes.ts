e649eed245827133b02cedafdc7cd790021e657f
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-23 17:53:08 -0500

    rutas mejoradas

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
 // import { Register } from './features/auth/register/register';
-import { Dashboard } from './features/admin/dashboard/dashboard';
+import { Dashboard as AdminDashboard } from './features/admin/dashboard/dashboard';
 import { DashboardWaiter } from './features/waiter/dashboard/dashboard.waiter';
 import { authGuard } from './core/guards/auth-guard';
 import { roleGuard } from './core/guards/role-guard';
 import { Users } from './features/admin/users/users';
 import { DashboardKitchen } from './features/kitchen/dashboard/dashboard.kitchen';
+import { Products } from './features/admin/products/products';
+import { Tables } from './features/admin/tables/tables';
+import { Reports } from './features/admin/reports/reports';
 
 export const routes: Routes = [
   {
@@ -16,32 +19,32 @@ export const routes: Routes = [
   },
 
   {
-    path: 'admin/users',
-    component: Users,
-    canActivate: [authGuard, roleGuard('ADMIN')]
-  },
-
-  {
     path: 'login',
     component: Login
   },
 
   {
+    path: 'admin',
+    canActivate: [authGuard, roleGuard('ADMIN')],
+    children: [
+      { path: '', component: AdminDashboard },
+      { path: 'users', component: Users },
+      { path: 'products', component: Products },
+      { path: 'tables', component: Tables },
+      { path: 'reports', component: Reports },
+    ],
+  },
+
+  {
     path: 'waiter',
-    component: Dashboard,
-    canActivate: [
-      authGuard,
-      roleGuard('MOZO')
-    ]
+    component: DashboardWaiter,
+    canActivate: [authGuard, roleGuard('MOZO')],
   },
 
   {
     path: 'kitchen',
     component: DashboardKitchen,
-    canActivate: [
-      authGuard,
-      roleGuard('COCINERO')
-    ]
+    canActivate: [authGuard, roleGuard('COCINERO')]
   },
 
   {
