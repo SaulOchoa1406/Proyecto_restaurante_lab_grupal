7f5c236c68febbac06409379000f89f8ab14d3e3
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-23 18:32:08 -0500

    clase para lectura libre y escritura solo del admin

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -10,3 +10,20 @@ class IsAdministrador(permissions.BasePermission):
             request.user.is_authenticated and
             request.user.rol == Usuario.Roles.ADMIN
         )
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Cualquier usuario autenticado puede leer (list/retrieve).
+    Solo el admin puede crear, editar o eliminar.
+    """
+
+    def has_permission(self, request, view):
+
+        if not request.user.is_authenticated:
+            return False
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user.rol == Usuario.Roles.ADMIN
