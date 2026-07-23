4cd29986ec4fda2f467347137d70b853343e192a
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-23 17:15:27 -0500

    plantilla order modificado

--- frontend/angular/src/app/core/models/order-detail.ts
+++ frontend/angular/src/app/core/models/order-detail.ts
@@ -2,7 +2,9 @@ import { Product } from "./product";
 
 export interface OrderDetail {
     id:number;
+    order:number;
     product:Product;
+    product_id?:number;
     amount:number;
     unit_price:number;
     subtotal:number;
--- frontend/angular/src/app/core/models/order.ts
+++ frontend/angular/src/app/core/models/order.ts
@@ -2,9 +2,12 @@ import { Table } from "./table";
 import { User } from "./user";
 import { OrderDetail } from "./order-detail";
 
+export type OrderStatus = | 'PENDIENTE' | 'PREPARANDO' | 'LISTO' | 'ENTREGADO' | 'PAGADO' | 'CANCELADO';
+
 export interface Order {
     id:number,
     table:Table;
+    table_id?:number;
     waiter:User;
     customer_name:string;
     customer_dni:string;
