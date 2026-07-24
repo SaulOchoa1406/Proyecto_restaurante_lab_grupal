9cfb3be8d1a9f40b714e200b437358a321d65ea3
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-23 22:26:25 -0500

    componente dashboard completo

--- frontend/angular/src/app/features/waiter/dashboard/dashboard.waiter.ts
+++ frontend/angular/src/app/features/waiter/dashboard/dashboard.waiter.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TableList } from '../table-list/table-list';
+import { TableDetail } from '../table-detail/table-detail';
+import { Table } from '../../../core/models/table';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [TableList, TableDetail],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class DashboardWaiter {}
+export class DashboardWaiter {
+  selectedTable = signal<Table | null>(null);
+
+  onTableSelected(table: Table): void {
+    this.selectedTable.set(table);
+  }
+
+  closeDetail(): void {
+    this.selectedTable.set(null);
+  }
+}
