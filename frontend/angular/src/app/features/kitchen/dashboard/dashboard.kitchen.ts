8127af5dc8ca4a87a044ef7964d67e90d7ac43f4
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-23 23:16:58 -0500

    componente dashboard de kitchen

--- frontend/angular/src/app/features/kitchen/dashboard/dashboard.kitchen.ts
+++ frontend/angular/src/app/features/kitchen/dashboard/dashboard.kitchen.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PendingOrders } from '../pending-orders/pending-orders';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [PendingOrders],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
