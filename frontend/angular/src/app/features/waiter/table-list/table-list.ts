1cf7adc299a55f7ce3c713a0708a37ea05c151a0
saulocaceresg <scaceresg@unsa.edu.pe>
2026-07-23 20:55:52 -0500

    table-list para las mesas y cargar datos

--- frontend/angular/src/app/features/waiter/table-list/table-list.ts
+++ frontend/angular/src/app/features/waiter/table-list/table-list.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Output, signal } from '@angular/core';
+import { Table } from '../../../core/models/table';
+import { TableService } from '../../../core/services/table';
+import { interval, startWith, Subscription, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-table-list',
@@ -6,4 +9,39 @@ import { Component } from '@angular/core';
   templateUrl: './table-list.html',
   styleUrl: './table-list.css',
 })
-export class TableList {}
+export class TableList {
+  @Output() tableSelected = new EventEmitter<Table>();
+
+  private readonly tableService = inject(TableService);
+  private subscription?: Subscription;
+
+  tables = signal<Table[]>([]);
+  loading = signal(true);
+  error = signal('');
+
+  ngOnInit(): void {
+    this.subscription = interval(7000)
+      .pipe(
+        startWith(0),
+        switchMap(() => this.tableService.list())
+      )
+      .subscribe({
+        next: (tables) => {
+          this.tables.set(tables);
+          this.loading.set(false);
+        },
+        error: () => {
+          this.error.set('No se pudieron cargar las mesas.');
+          this.loading.set(false);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  select(table: Table): void {
+    this.tableSelected.emit(table);
+  }
+}
